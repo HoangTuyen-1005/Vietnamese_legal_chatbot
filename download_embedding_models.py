43c562fac924d04e9d4a19cba579fe834afaaf72
HoangTuyen-1005 <tuyentruong10062005@gmail.com>
2025-10-17 20:50:10 +0700

    add new file download_embedding_models.py and change directory model save folder in download_models.py

--- download_embedding_models.py
+++ download_embedding_models.py
@@ -1,9 +1,9 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="vilm/vinallama-7b-chat-GGUF",
+    repo_id="caliex/all-MiniLM-L6-v2-f16.gguf",
     allow_patterns=["*.gguf"],
     local_dir="models",
 )
 
-print("Model downloaded to ./model")
+print("Embedding model downloaded to ./models")
